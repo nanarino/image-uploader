74ab5e6ec5eb3955551e135a5be84e300e5241da
☘ <kogawananari@gmail.com>
2023-05-12 23:07:43 +0800

    :bug:
    上游的试验宏defineModel现在允许启用`defineModel`

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue({
+      script: {
+        defineModel: true
+      }
+    })
+  ]
 })
