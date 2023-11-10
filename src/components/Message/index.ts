e088b81fda31eed980d526ace750bc2f3e412e4e
☘ <kogawananari@gmail.com>
2023-11-10 22:18:37 +0800

    :sparkles: 额外的样式

--- src/components/Message/index.ts
+++ src/components/Message/index.ts
@@ -8,17 +8,32 @@ export class Launcher {
     parentElement.append(this.queue)
   }
   async emit(detail: string | {
+    /**
+     * 内容 相当于直接传string
+     */
     content?: string
+    /**
+     * 主题 4种 其他非空值等效于传primary
+     */
     primary?: "success" | "danger" | "warning" | "primary"
+    /**
+     * 持续时间
+     */
     duration?: number
+    /**
+     * 额外的样式 需要设置primary后才生效
+     */
+    style?: Record<string, string>
   }) {
     const msg = document.createElement("div")
     let duration: number = 2000
     let primary: string = ""
+    let style: Record<string, string> = {};
     let content = detail ?? "☘"
     if (typeof content != 'string') {
       duration = content.duration ?? 2000
       primary = `${content.primary ?? ""}`
+      style = content.style ?? {};
       content = `${content.content ?? "☘"}`
     }
     msg.innerHTML = `
@@ -27,7 +42,9 @@ export class Launcher {
       </div>
     `
     if (primary) {
-      (msg.firstElementChild as HTMLDivElement).dataset.primary = primary
+      const p = (msg.firstElementChild as HTMLDivElement)
+      p.dataset.primary = primary
+      Object.entries(style).map(x => p.style.setProperty(...x))
     }
     this.queue.appendChild(msg)
     msg.style.height = `${msg.offsetHeight}px`
