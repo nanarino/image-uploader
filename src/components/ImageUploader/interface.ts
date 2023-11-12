b5785fdc3ff686af29bea34f079fbf848a139fb5
☘ <kogawananari@gmail.com>
2023-11-13 02:24:41 +0800

    :rocket:

--- src/components/ImageUploader/interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Gazo  {
-  meta?: Record<string, any>
-  url?: string
-  name: string
-  size: number
-  type: string
-}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import './assets/iconfont/iconfont.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
