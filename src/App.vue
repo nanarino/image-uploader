d925ae8dfc0ff271310decc109f6aab744e8bfd4
☘ <kogawananari@gmail.com>
2023-11-12 15:16:27 +0800

    :hammer:

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,9 @@ const change = async (item: {
 }
 
 void async function init() {
-  const res = await fetch('/Kirby.json')
-  const Kirby = await res.json()
+  const res = await fetch(`${import.meta.env.BASE_URL}Kirby.json`)
+  const Kirby = await res.json() as Gazo
+  if(Kirby.url) Kirby.url = Kirby.url.replace(/^\//, import.meta.env.BASE_URL)
   imgs.push(Kirby)
 }()
 </script>
