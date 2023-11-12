fce37f98a0a495b209e8ab1b2613b76099c0a256
☘ <kogawananari@gmail.com>
2023-11-13 02:33:58 +0800

    :bug:
    This assignment will throw because "imgs" is a constant

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { ref, reactive, nextTick } from 'vue'
+import { ref, nextTick } from 'vue'
 import ImageUploader from '@/components/ImageUploader/index.vue'
 import message from '@/components/Message'
 import { Gazo } from '@/components/ImageUploader/interface'
 
-const imgs = reactive<Gazo[]>([])
+/**
+ * 如果使用 `reactive` 会有报错：
+ *  `This assignment will throw because "imgs" is a constant`
+ * 但是不影响使用 也可以不使用 `v-model`
+ */
+const imgs = ref<Gazo[]>([])
 const ImageUploaderRef = ref({ size: 0 })
 
 const change = async (item: {
@@ -21,7 +26,7 @@ void async function init() {
   const res = await fetch(`${import.meta.env.BASE_URL}Kirby.json`)
   const Kirby = await res.json() as Gazo
   if(Kirby.url) Kirby.url = Kirby.url.replace(/^\//, import.meta.env.BASE_URL)
-  imgs.push(Kirby)
+  imgs.value.push(Kirby)
 }()
 </script>
 
