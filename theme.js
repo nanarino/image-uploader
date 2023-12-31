fe316d78900ebb4a4805a9f33367cf484a39660e
☘ <kogawananari@gmail.com>
2023-12-31 12:21:00 +0800

    :rocket:

--- theme.js
+++ theme.js
@@ -2,6 +2,12 @@
  * 获取系统是否暗黑模式
  * 这个代码要放在body之前 否则可能会有闪烁
  */
-document.documentElement.dataset["theme"] = ["dark", "light"][
-  +!window.matchMedia?.("(prefers-color-scheme: dark)")?.matches
-];
+Object.defineProperty(window, "theme", {
+  get: () =>
+    localStorage.getItem("theme") ??
+    ["dark", "light"][
+        +!window.matchMedia?.("(prefers-color-scheme: dark)")
+            ?.matches
+    ],
+});
+document.documentElement.dataset["theme"] = window.theme;
