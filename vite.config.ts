835f2e7a42ffd5089f589eddb27f02b01c793d27
☘ <kogawananari@gmail.com>
2024-01-31 00:15:34 +0800

    :pushpin:

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import alias from '@astropub/config-to-alias/vite'
+import alias from '@holy-two/vite-plugin-alias'
 
 // https://vitejs.dev/config/
 export default defineConfig({
